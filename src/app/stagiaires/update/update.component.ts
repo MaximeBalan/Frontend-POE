60fc14eb384ae42e1d306ff38beef2edcb488b7a
Oriane <lieoriane@gmail.com>
2023-01-05 16:49:12 +0100

    Le formulaire de update est pré-rempli

--- src/app/stagiaires/update/update.component.ts
+++ src/app/stagiaires/update/update.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { StagiaireModel } from 'src/app/core/models/stagiaire-model';
 import { StagiaireService } from 'src/app/core/services/stagiaire-service';
 import { DateLessThan } from 'src/app/core/validators/date-less-than';
@@ -12,54 +12,77 @@ import { DateLessThan } from 'src/app/core/validators/date-less-than';
 })
 export class UpdateComponent implements OnInit {
 
+  public stagiaire: StagiaireModel |null = null;
   public addStagiaireForm!: FormGroup; //Groupe de Contrôles de formulaire
   
   constructor(
     private formBuilder: FormBuilder, //permet de construire un formulaire
     private stagiaireService: StagiaireService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: StagiaireService
   ) { }
 
   ngOnInit(): void {
-    this.addStagiaireForm = this.formBuilder.group({
-      lastName: [
-        '', //Default value (here empty)mais ce n'est pas un placeholder!!!
-        
-        //tableau pas obligatoire, sauf si on a plusieurs validators !
-        [
-          Validators.required //le contrôle doit impérativement être défini avec une valeur non nulle
-        ]
-      ],
-      firstName: [
-        '',
-        Validators.required
-      ],
-      gender: [
-        'M'
-      ],
-      birthDate : [
-        '',
-        [
-          Validators.required,
-          DateLessThan.dateLessThan()
-        ]
-      
-      ],
-      phoneNumber: [
-        '',
-        [
-          Validators.pattern(/^\+(?:[0-9]●?){6,14}[0-9]$/)
-        ]
-        
-      ],
-      email : [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
-        ]
-      ]
-    })        
+    
+      //pour récupérer des détails/données d'une autre page
+      this.route.paramMap.subscribe(
+        (routeParams) => {
+          console.log(`Detail got ${routeParams.get('id')}`); //id est le même nom que dans le path de detail
+          
+          try {
+            this.service.findOne(+routeParams.get('id')!)
+              .subscribe((stagiaire: StagiaireModel)=> {
+                this.stagiaire = stagiaire;
+
+                // Here comes the Trainee
+                this.addStagiaireForm = this.formBuilder.group({
+                  lastName: [
+                    this.stagiaire.lastName, //Default value (here empty)mais ce n'est pas un placeholder!!!
+                    
+                    //tableau pas obligatoire, sauf si on a plusieurs validators !
+                    [
+                      Validators.required //le contrôle doit impérativement être défini avec une valeur non nulle
+                    ]
+                  ],
+                  firstName: [
+                    this.stagiaire.firstName,
+                    Validators.required
+                  ],
+                  gender: [
+                    this.stagiaire.gender
+                  ],
+                  birthDate : [
+                    this.stagiaire.birthDate,
+                    [
+                      Validators.required,
+                      DateLessThan.dateLessThan()
+                    ]
+                  
+                  ],
+                  phoneNumber: [
+                    this.stagiaire.phoneNumber,
+                    [
+                      Validators.pattern(/^\+(?:[0-9]●?){6,14}[0-9]$/)
+                    ]
+                    
+                  ],
+                  email : [
+                    this.stagiaire.email,
+                    [
+                      Validators.required,
+                      Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
+                    ]
+                  ]
+                })      
+              })
+            console.log(JSON.stringify(this.stagiaire));
+          } catch (error) {
+            this.router.navigate(['/', 'stagiaires']);
+  
+          }
+        }
+      ) 
   }
 
   public onSubmit(): void {
@@ -70,8 +93,6 @@ export class UpdateComponent implements OnInit {
           this.router.navigate(['/', 'stagiaires']);
           // this.addStagiaireForm.reset();
     });
-
   }
-
 }
 
