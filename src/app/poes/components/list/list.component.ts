f7149dc851ce3c62bd09088af99f7dde88077c0c
Maxime Balan <maxime.balan@gmail.com>
2023-01-05 13:41:17 +0100

    Add deletePoe

--- src/app/poes/components/list/list.component.ts
+++ src/app/poes/components/list/list.component.ts
@@ -1,6 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Poe } from 'src/app/core/models/poe';
 import { PoeService } from 'src/app/poes/services/poe/poe.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list-poe',
@@ -14,6 +16,7 @@ export class ListComponent implements OnInit {
     private PoesService: PoesService,
     private snackBar: MatSnackBar*/
     private poeService: PoeService,
+    private snackBar: MatSnackBar
   ) {}
 
 
@@ -21,7 +24,25 @@ export class ListComponent implements OnInit {
     this.poeService.findAll()
     .subscribe((poes: Poe[])=> {
       this.poes = poes;
-    }); 
+    });
+  }
+
+  public onDelete(poe: Poe):void{
+    this.poeService.delete(poe)
+      .subscribe((response : HttpResponse<any>)=>{
+        //supprimer la ligne dans this.stagiaires
+        this.poes.splice(
+          this.poes.findIndex((obj: Poe) => obj.id === poe.id),
+          1
+        );
+        this.snackBar.open(
+          `le stagiaire ${poe.id} a été supprimé`,
+          'Compris', //le nom à cliquer pour fermer la notification d'alerte
+          {
+            duration: 2500
+          }
+        )
+      })
   }
 
 }
