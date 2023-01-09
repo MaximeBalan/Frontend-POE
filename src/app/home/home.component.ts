933e11fa4a05a6bd783e74eefa43ba712150eef5
Maxime Balan <maxime.balan@gmail.com>
2023-01-09 09:20:46 +0100

    update home: add poes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Poe } from '../core/models/poe';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PoeService } from '../poes/services/poe/poe.service';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  public poes:Poe[] = [];
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: PoeService
+  ) {}
+
+  ngOnInit(): void {
+    this.service.findAll()
+      .subscribe((poes: Poe[])=> {
+        this.poes = poes;
+      });
+  }
+
+  public goToList():void{
+    this.router.navigate(['/poes']);
+  }
 
 
 }
