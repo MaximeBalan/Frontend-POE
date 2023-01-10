bbe01ba756d988f2a64db147dab59c9734d7f775
Damien <damienweishar@gmail.com>
2023-01-10 11:33:53 +0100

    Add located strings for TranslateModule

--- src/app/intl/intl.module.ts
+++ src/app/intl/intl.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { intlProvider } from './services/intl.service';
+import { intlProvider, IntlService } from './services/intl.service';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forRoot(
+      {
+        loader: {
+        provide: TranslateLoader,
+        useFactory: IntlService.httpLoaderFactory,
+        deps: [
+          HttpClient
+        ]
+      }
+    }
+    ) //pour que le token soit fourni à la racine de l'application
+  ],
+  exports:[
+    TranslateModule
   ],
   providers: [
     intlProvider //on rajoute un fournisseur
--- src/app/intl/services/intl.service.ts
+++ src/app/intl/services/intl.service.ts
@@ -1,25 +1,67 @@
-import { APP_INITIALIZER, Injectable } from '@angular/core';
+import { LOCATION_INITIALIZED } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { APP_INITIALIZER, Injectable, Injector } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntlService {
-
+  private _language: string = '';
+  private _translateService!:TranslateService;
   constructor() { }
 
-  //méthode qu'on va appeler dès qu'Angular se lance et avant l'affichage
-  init(){
-    return new Promise<void>((resolve:any)=> {
-      console.log(`I'm running`);
-      resolve();
-     });
+  get language(): string{
+    return this._language
+  }
+  set language(language: string){
+    this._language = language;
   }
 
+  //méthode qu'on va appeler dès qu'Angular se lance et avant l'affichage
+  //pour charger les sources qu'Angular va utiliser
+  init(translateService: TranslateService, injector: Injector){
+    return new Promise<void>((resolve:any)=> {
+      injector.get(
+        LOCATION_INITIALIZED,
+        Promise.resolve(null) //if TOKEN was not provided
+      ).then(()=>{ // Angular says "Iim ok with my own ressources, do what you need"
+        const userLanguage: string = window.navigator.language.split('-')[0]; // fr-FR => fr | FR => fr
+        this._language = /(fr|en|de|it|es)/gi.test(userLanguage) ? userLanguage : 'fr';
+
+        this._translateService = translateService;
+
+        this._switchLanguage().subscribe(()=> {
+          console.log(`Location was loaded for ${this._language}!!`);
+          resolve();
+        })       
+      })
+    });
+  }
+
+  private _switchLanguage(): Observable<any>{
+    return this._translateService.use(this._language);
+  }
   
-  public static appInitializer(intlService: IntlService){
+  public static appInitializer(
+    //même ordre que dans les deps de intlProvider
+    intlService: IntlService,
+    translateService: TranslateService,
+    injector: Injector
+    ){
     return(): Promise<void> => {
-      return intlService.init();
+      return intlService.init(translateService, injector);
     }
+  }
+
+  //récupère tous les fichiers dans i18n avec l'extension par .json
+  public static httpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader { 
+    return new TranslateHttpLoader( 
+      httpClient, 
+      './assets/i18n/', 
+      '.json' ); 
   }
 }
 
@@ -29,7 +71,9 @@ export const intlProvider = {
   useFactory: IntlService.appInitializer, //appel à une fonction qui fait appel à l'init
   deps: [
     //instancie les classes ci-dessous pour ensuite pouvoir les utiliser 
-    IntlService 
+    IntlService,
+    TranslateService,
+    Injector
   ],
   multi:true //autorise à avoir plusieurs services à fournir le token au démarrage de l'application
 }
