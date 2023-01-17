feff39062319c331b3cd12711f2ab9645918b7c1
Damien <damienweishar@gmail.com>
2023-01-17 11:55:40 +0100

    ADD searchBar icon + icons in black

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -13,6 +13,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import {MatSortModule} from '@angular/material/sort';
 import {MatExpansionModule} from '@angular/material/expansion';
 
+
 @NgModule({
   declarations: [],
   imports: [
