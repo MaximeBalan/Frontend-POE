d1f64e61b932ea92ed4b5c4f3acfee4f0e3ff914
Maxime Balan <maxime.balan@gmail.com>
2023-01-25 15:04:33 +0100

    start drag and drop

--- src/app/surveys/add/add.component.ts
+++ src/app/surveys/add/add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuestionModel } from 'src/app/core/models/question';
+import { QuestionService } from 'src/app/core/services/question-service';
+import { SurveyService } from 'src/app/core/services/survey-service';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+  public questions:QuestionModel[] = [];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private surveyService: SurveyService,
+    private questionService: QuestionService
+  ) { }
 
   ngOnInit(): void {
+    this.questionService.findAll()
+      .subscribe((questions: QuestionModel[])=> {
+        this.questions = questions;
+      });
   }
-
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
+  }
 }
