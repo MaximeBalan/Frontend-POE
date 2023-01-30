cd1e733afd0badb32a382cbb14b41b55f7b8c792
Maxime Balan <maxime.balan@gmail.com>
2023-01-30 16:44:01 +0100

    creating 2 list drag and drop

--- src/app/surveys/add/add.component.ts
+++ src/app/surveys/add/add.component.ts
@@ -1,32 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop,moveItemInArray,transferArrayItem,} from '@angular/cdk/drag-drop';
+import { Component} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { QuestionModel } from 'src/app/core/models/question';
+import { SurveyModel } from 'src/app/core/models/survey-model';
 import { QuestionService } from 'src/app/core/services/question-service';
-import { SurveyService } from 'src/app/core/services/survey-service';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
-export class AddComponent implements OnInit {
-  public questions:QuestionModel[] = [];
+export class AddComponent {
+  title = 'dropzone';
+  public questionAvailable: QuestionModel[] = [];
+  public questionSelected: QuestionModel[] = [];
+  public surveys: SurveyModel[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private surveyService: SurveyService,
     private questionService: QuestionService
   ) { }
-
   ngOnInit(): void {
-    this.questionService.findAll()
+        this.questionService.findAll()
       .subscribe((questions: QuestionModel[])=> {
-        this.questions = questions;
+        this.questionAvailable = questions;
       });
   }
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
+
+  drop(event: CdkDragDrop<QuestionModel[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
+
+
+
+  // drop(event: CdkDragDrop<QuestionModel[]>) {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  //   } else {
+  //     transferArrayItem(
+  //       event.previousContainer.data,
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex,
+  //     );
+  //   }
+  // }
 }
